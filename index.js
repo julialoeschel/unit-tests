3c0cc8919bcb505fde9d59cd621045cc5a080efb
Julia Loeschel <juloeschel@gmail.com>
2021-10-18 11:42:00 +0200

    repace name add

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ function getGreetin(name = 'you') {
   return `Hello ${name}!` //template literal
   //return 'hello ' + name + '!'
 }
-console.log(getGreetin('Julia'))
+
+function replaceName(text, name, newName) {
+  return text.replaceAll(name, newName)
+}
+console.log(replaceName('hello Jane skjd idid Jane', 'Jane', 'julia'))
